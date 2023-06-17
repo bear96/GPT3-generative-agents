7df23a5fa9dd2a14042bd183367ffecaa6fca81c
Aritra Majumdar <aritram21@vt.edu>
2023-06-17 16:44:36 -0400

    new

--- dorito/__init__.py
+++ dorito/__init__.py
@@ -1,8 +1,8 @@
 # dorito/__init__.py
 
-from memory import MemoryObject, MemoryStream
-from generative_agent import GenerativeAgent
-from utils import get_embedding,get_importance,get_importances,get_completion,cosine_similarity
+from .memory import MemoryObject, MemoryStream
+from .generative_agent import GenerativeAgent
+from .utils import get_embedding,get_importance,get_importances,get_completion,cosine_similarity
 
 __all__ = [
     "GenerativeAgent", 
