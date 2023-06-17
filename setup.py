ee19a721af4fe253c5b6af6f7f333c471a8ec54f
Aritra Majumdar <aritram21@vt.edu>
2023-06-17 16:36:48 -0400

    new

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 from setuptools import setup, find_packages
 
-# Read the requirements from requirements.txt file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='dorito',
     version='0.1',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires="openai",
 )
